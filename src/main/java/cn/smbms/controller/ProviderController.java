6dd5fe080e6036b7f353aa8716adbfde9fb3e231
HouWei96 <9647437@qq.com>
2017-11-11 13:21:28 +0800

    超市订单管理系统

---
+++ src/main/java/cn/smbms/controller/ProviderController.java
@@ -0,0 +1,25 @@
+package cn.smbms.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import cn.smbms.pojo.Provider;
+import cn.smbms.pojo.User;
+import cn.smbms.service.ProviderService;
+import cn.smbms.tools.Page;
+
+@Controller
+public class ProviderController {
+	
+}
